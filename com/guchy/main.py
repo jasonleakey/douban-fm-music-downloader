929e115bdf59187bd26a62aea397c0a611599cb4
Jason Huang <yetianhuang.cs@gmail.com>
2014-11-28 10:02:00 -0800

    first commit

---
+++ com/guchy/main.py
@@ -0,0 +1,28 @@
+__author__ = 'jasonhuang'
+
+import urllib.request as ur
+import json, os, sys
+
+os.chdir(sys.path[0])
+
+t = r'http://douban.fm/j/mine/playlist?type=n&channel='
+
+for i in range(24):
+    url = t + str(i)
+    print(url)
+    a = ur.urlopen(url).read().decode().replace('\\', '')
+    a = json.loads(a)
+    for i in a['song']:
+        filename = i['artist'] + '-' + i['albumtitle'] + '.mp3'
+        print('Downloading:', 'Artist: ' + i['artist'], 'Song: ' + i['albumtitle'], 'URL: ' + i['url'],
+              sep='\n', end = '\n\n')
+        try:
+            if os.path.exists(filename):
+                print('Existing')
+                break
+            ur.urlretrieve(i['url'], filename)
+            if os.path.getsize(filename) < 300:
+                os.system('del ' + filename)
+        except Exception as a:
+            print (a)
+            pass
