2d9973556f55e981e470b07f42fe1f46b6b14641
Jason Huang <yetianhuang.cs@gmail.com>
2014-11-29 13:44:21 -0800

    aaaa

--- com/guchy/main.py
+++ com/guchy/main.py
@@ -18,14 +18,19 @@ def reporthook(blocknum, blocksize, totalsize):
         sys.stdout.write("read %d\n" % (readsofar,))
 
 os.chdir(sys.path[0])
-t = r'http://douban.fm/j/mine/playlist?type=n&channel='
+t = r'http://douban.fm/j/mine/playlist?from=mainsite&type=n&channel='
+CHANNEL_CHINESE = 1
+CHANNEL_ENGLISH = 2
+CHANNEL_70 = 3
+CHANNEL_80 = 4
+CHANNEL_90 = 5
 logging.basicConfig(filename="main.log", level=logging.DEBUG)
 
 
 # set network timeout as 30 secs.
 # download a file should not take that long.
 socket.setdefaulttimeout(30)
-for i in range(24):
+for i in [ CHANNEL_CHINESE, CHANNEL_ENGLISH, CHANNEL_70, CHANNEL_80, CHANNEL_90 ]:
     url = t + str(i)
     logging.debug("\n")
     logging.debug(url)
